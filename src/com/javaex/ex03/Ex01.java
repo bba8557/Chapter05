f6d3d68f0cadd06490cda0bdbc235194f0ef1f4c
bba8557 <bba8557@naver.com>
2022-05-08 22:24:57 +0900

    22.05.08

--- src/com/javaex/ex03/Ex01.java
+++ src/com/javaex/ex03/Ex01.java
@@ -10,17 +10,22 @@ public class Ex01 {
 
 	public static void main(String[] args) throws IOException {
 		// 1. split(",") 사용
-		
+
 		InputStream in = new FileInputStream("C:\\javaStudy\\file\\PhoneDB.txt");
 		InputStreamReader isr = new InputStreamReader(in);
 		BufferedReader br = new BufferedReader(isr);
-		
 
+		while (true) {
+			String str = br.readLine();
+			str.split(",");
+			if (str == null) {
+				
+				break;
+			}
+			System.out.println(str);
 
-	
-			
-		
-		
+		}
+
 		br.close();
 	}
 }
